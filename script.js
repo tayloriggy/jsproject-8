11ec05efba5f765796803aefd5a4a415bc774b75
Iggy36 <42382841+tayloriggy@users.noreply.github.com>
2021-11-15 14:34:44 -0600

    Created index.html, style.css, script.js for To Do App

---
+++ script.js
@@ -0,0 +1,11 @@
+/*
+A user must be able to add and delete a todo item, from the UI and from your storage.
+-Hint: use JavaScript localStorage to store the todos.
+
+You can store your Todos in an array, but keep in mind that localStorage can only store strings, 
+so that means you'll have to convert your array of todos into a string before you can store it. 
+And when you retrieve it, you have to convert it from a string back to an array. 
+Those are all things you can do with JavaScript, so make sure to look those up.
+
+
+*/
