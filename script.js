e71499ecee0ce909b4aa5795a6013e71d63901b1
Iggy36 <42382841+tayloriggy@users.noreply.github.com>
2021-11-15 15:34:32 -0600

    script.js, created items in localStorage, created array of same items and stored in a variable

--- script.js
+++ script.js
@@ -7,5 +7,25 @@ so that means you'll have to convert your array of todos into a string before yo
 And when you retrieve it, you have to convert it from a string back to an array. 
 Those are all things you can do with JavaScript, so make sure to look those up.
 
+Steps:
+
+1. Create items in localStorage to be stored.
+2. Create a list of todos in an array.
+3. In index.html, create an input box to add a task. Create an add button at end.
+4. Create a delete button to delete the task.
+5. 
 
 */
+
+localStorage.setItem("task", "Wash the dishes");
+localStorage.setItem("task", "Do laundry");
+localStorage.setItem("task", "Go to the grocery store");
+localStorage.setItem("task", "Cook food");
+localStorage.setItem("task", "Vacuum");
+localStorage.setItem("task", "Mow the lawn");
+
+let tasks = ["Wash the dishes", "Do laundry", "Go to the grocery store", "Cook food", "Vacuum", "Mow the lawn"];
+console.log(tasks);
+
+
+
